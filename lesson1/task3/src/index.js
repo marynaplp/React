a9436ae34308b210db057b95527099b135f3ebad
Maryna <maryna.pylypchenko@gmail.com>
2020-07-24 12:02:55 +0300

    co

---
+++ lesson1/task3/src/index.js
@@ -0,0 +1,12 @@
+import './styles.css';
+
+const rootElement = document.querySelector('#root');
+
+const element = ( <
+    div className = "greeting" >
+    <
+    div className = "greeting__title" > Hello, world! < /div>  <
+    div className = "greeting__text" > I / 'm learning React</div>  < /
+    div > );
+
+ReactDOM.render(element, rootElement)
