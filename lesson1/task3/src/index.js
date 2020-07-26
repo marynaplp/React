c22cf614153dd640c1bb12fac3d23c097b270dff
Maryna <maryna.pylypchenko@gmail.com>
2020-07-26 22:36:41 +0300

    co

--- lesson1/task3/src/index.js
+++ lesson1/task3/src/index.js
@@ -3,11 +3,11 @@ import './styles.css'
 const rootElement = document.querySelector('#root');
 
 const element = ( <
-    div className = "greeting" >
-    <
-    div className = "greeting__title" > Hello, world! < /div>  <
-    div className = "greeting__text" > I 'm learning React</div>  <
-    /div>`
+        div className = "greeting" >
+        <
+        div className = "greeting__title" > Hello, world! < /div>  <
+        div className = "greeting__text" > I 'm learning React</div>  < /
+        div > `
 );
 
 ReactDOM.render(element, rootElement)
