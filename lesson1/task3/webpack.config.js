1feb03d357a62926115e6c92b54b71d1e2f1ea39
Maryna <maryna.pylypchenko@gmail.com>
2020-07-27 13:54:19 +0300

    co

--- lesson1/task3/webpack.config.js
+++ lesson1/task3/webpack.config.js
@@ -1,20 +1,29 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+  const path = require('path');
 
-module.exports = {
-    module: {
-        rules: [{
-                test: /.js$/,
-                use: ['babel-loader']
-            },
-            {
-                test: /.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
-}
+  const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+  module.exports = {
+      entry: "./src/index.js",
+      output: {
+          path: path.resolve(__dirname, "review_build"),
+          filename: "review_build.js",
+      },
+
+      module: {
+          rules: [{
+                  test: /.js$/,
+                  use: ["babel-loader"],
+              },
+              {
+                  test: /.css$/,
+                  use: ["style-loader", "css-loader"],
+              },
+          ],
+      },
+      plugins: [
+          new HtmlWebpackPlugin({
+              template: "./src/index.html",
+          }),
+      ],
+  };
