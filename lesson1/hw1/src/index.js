62d8ae995adc78b28ccea4737aecab55e0e9fe49
Maryna <maryna.pylypchenko@gmail.com>
2020-07-27 16:49:03 +0300

    co

---
+++ lesson1/hw1/src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.querySelector('#root');
+
+const element = ( 
+<h1 class="title">Todo List</h1>
+);
+ReactDOM.render(element, rootElement)
