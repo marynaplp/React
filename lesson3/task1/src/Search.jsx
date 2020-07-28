2770a12345b8b3e587bcc31ec2eb868a6fb902d4
Maryna <maryna.pylypchenko@gmail.com>
2020-07-28 14:51:02 +0300

    co

---
+++ lesson3/task1/src/Search.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.querySelector('#root');
+
+<div class="search">
+    <h1 class="search__title">Hello, Tom. What to search for you?</h1>
+    <div class="search__field">
+        <input type="text" class="search__input" />
+        <button class="search__button">Search</button>
+    </div>
+</div>
+ReactDOM.render(element, rootElement)
---
+++ lesson3/task2/src/Search.jsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.querySelector('#root');
+
+const element = (
+    <div class="search">
+        <h1 class="search__title">Hello, Tom. What to search for you?</h1>
+        <div class="search__field">
+            <input type="text" class="search__input" />
+            <button class="search__button">Search</button>
+        </div>
+    </div>
+);
+ReactDOM.render(element, rootElement)
