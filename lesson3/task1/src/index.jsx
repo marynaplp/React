0647dfbc44ce89cf65c6b35564bfa06ba764525f
Maryna <maryna.pylypchenko@gmail.com>
2020-07-28 16:32:21 +0300

    co

--- lesson3/task1/src/index.jsx
+++ lesson3/task1/src/index.jsx
@@ -4,4 +4,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const rootElement = document.querySelector('#root');
-ReactDOM.render( < Search name="Tom" /> , rootElement)
+ReactDOM.render( <Search name="Tom" /> , rootElement)
