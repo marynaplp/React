21efd275d7de916e6794260bd55c012cbd6c3682
Maryna <maryna.pylypchenko@gmail.com>
2020-07-28 17:12:54 +0300

    co

--- lesson3/task1/src/Search.jsx
+++ lesson3/task1/src/Search.jsx
@@ -1,14 +1,16 @@
+import React from "react";
+import "./search.scss";
 
 const Search =(props) => {
     return (
-<div class="search">
-    <h1 class="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
-    <div class="search__field">
-        <input type="text" class="search__input" />
-        <button class="search__button">Search</button>
-    </div>
-</div>
-)
+      <div className="search">
+        <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+        <div className="search__field">
+          <input type="text" className="search__input" />
+          <button className="search__button">Search</button>
+        </div>
+      </div>
+    );
 }
 
 export default Search
--- lesson3/task1/src/index.jsx
+++ lesson3/task1/src/index.jsx
@@ -1,8 +1,9 @@
-import "./styles.scss";
-import "./search.scss";
+
 import React from "react";
 import ReactDOM from "react-dom";
-import Search from "./search.jsx"
+import Search from "./search.jsx";
+import "./index.scss";
 
-const rootElement = document.querySelector('#root');
-ReactDOM.render( <Search name="Tom" /> , rootElement)
+const rootElem = document.querySelector("#root");
+
+ReactDOM.render(<Search name="Tom" />, rootElem);
