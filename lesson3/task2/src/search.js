a8ce2cb0283eda3dfa03f8ad68ad5bc1336a0b69
Maryna <maryna.pylypchenko@gmail.com>
2020-07-28 17:59:20 +0300

    co

--- lesson3/task2/src/search.js
+++ lesson3/task2/src/search.js
@@ -18,7 +18,7 @@ export default class Search extends Component {
   render() {
     return (
       <div className="search">
-        <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+        <h1 className="search__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
         <div className="search__field">
           <input type="text" className="search__input" />
           <button className="search__button">Search</button>
