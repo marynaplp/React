387988b1fb526859ebb1d9485098bedeb434690f
Maryna <maryna.pylypchenko@gmail.com>
2020-07-29 18:01:25 +0300

    co

--- lesson3/hw1/src/Greeting.js
+++ lesson3/hw1/src/Greeting.js
@@ -3,10 +3,10 @@ import React from "react";
 const getAge = (date) => parseInt((new Date().getTime()-new Date(date)/(24*3600*1000)))
 
 function Greeting(props) {
-    return (
-    <div className="greeting">{`My name is ${props.firstName}${props.lastName}.I'm ${getAge(props.birthDate)}years old`}
+    console.log(props)
+    return <div className="greeting">{`My name is ${props.firstName}${props.lastName}.I'm ${getAge(props.birthDate)}years old`}
     </div>
-    )
+    
 };
 
 
