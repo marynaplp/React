a8132ee6c0134efb18e6ffee611376966b557e60
Maryna <maryna.pylypchenko@gmail.com>
2020-07-29 21:02:20 +0300

    co

---
+++ lesson3/hw2/src/Profile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./index.scss";
+import moment from 'moment';
+
+
+const birhDateUser = (date) => moment(date).format("DD MMM YY");
+
+const Profile = (props) => {
+  // console.log(props);
+  return (
+    <div>
+      <div className="profile__name">
+        {`${props.userData.firstName} ${props.userData.lastName}`}
+      </div>
+      <div className="profile__birth">
+        {`Was born ${birhDateUser(props.userData.birthDate)} in ${
+          props.userData.birthPlace
+        }`}
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
