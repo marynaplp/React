7cdb4b017e35ba051a6e627f5b4d49d5a618c266
Maryna <maryna.pylypchenko@gmail.com>
2020-07-30 19:16:53 +0300

    co

--- lesson1/task1/index.js
+++
@@ -1,7 +0,0 @@
-const rootElement = document.querySelector('#root');
-const greetingElem = React.createElement(
-    'div', { className: "greeting" },
-    'Hello, React!'
-);
-
-ReactDOM.render(greetingElem, rootElement);
