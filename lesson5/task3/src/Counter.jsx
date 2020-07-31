94064e66d372e04572e2bedd1a124ee97a94cf71
Maryna <maryna.pylypchenko@gmail.com>
2020-07-31 21:06:54 +0300

    co

---
+++ lesson5/task3/src/Counter.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+    this.decrement = this.decrement.bind(this);
+  }
+  decrement() {
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  }
+  increment =() => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  }
+  reset = () => {
+      this.setState({
+      counter: 0
+    });
+  }
+
+  render() {
+    return (
+      <div class="counter">
+        <button data-action="decrease" className="counter__button" onClick={this.decrement} >
+          -</button>
+        <span class="counter__value" onClick={()=> this.reset()}>
+             {this.state.counter} </span>
+        <button data-action="increase" class="counter__button"onClick={this.increment}
+        >+</button>
+      </div>
+    );
+  }
+}
+export default Counter;
