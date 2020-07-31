3c02074285c36418e2e61a9137f6c0f4073de6ed
Maryna <maryna.pylypchenko@gmail.com>
2020-07-31 17:03:40 +0300

    commit

---
+++ lesson5/task2/src/GoodButton.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+class GoodButton extends React.Component{
+    handleClick(e) {
+        console.log(e.target.textContent);
+        console.log(e);
+        alert("Good job");
+    }
+    render(){
+return (
+    <button className="fancy-button" onClick={this.handleClick}>Click me!</button>
+);
+}
+}
+export default GoodButton;
