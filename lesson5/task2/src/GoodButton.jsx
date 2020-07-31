d3f7e08bbc82d2dbac8bd24d75dc920e2d0a49f6
Maryna <maryna.pylypchenko@gmail.com>
2020-07-31 21:32:00 +0300

    co

--- lesson5/task2/src/GoodButton.jsx
+++ lesson5/task2/src/GoodButton.jsx
@@ -1,16 +1,18 @@
 
-import React from "react";
+import React, { Component } from "react";
 
-class GoodButton extends Component{
+class GoodButton extends Component {
     handleClick(e) {
-        console.log(e.target.textContent);
-        console.log(e);
         alert(e.target.textContent);
     }
-    render(){
-return (
-    <button className="fancy-button" onClick={this.handleClick}>Click me!</button>
-);
+
+    render() {
+        return (
+            <button className="fancy-button" onClick={this.handleClick}>
+                Click me!
+            </button>
+        );
+    }
 }
-}
+
 export default GoodButton;
