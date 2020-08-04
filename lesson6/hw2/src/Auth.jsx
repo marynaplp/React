a6bab7c36937c2edc3030822300ea454b4061466
Maryna <maryna.pylypchenko@gmail.com>
2020-08-04 17:31:15 +0300

    co

--- lesson6/hw2/src/Auth.jsx
+++ lesson6/hw2/src/Auth.jsx
@@ -1,52 +1,56 @@
 // опишите компонету котороя отображает компоненту Greeting and one of componets GuestGreeting/ UserGreeting depending on user status
 import React, { Component } from "react";
-import Spinner from "./Spinner.jsx"
-import Login from "./Login"
-import Logout from "./Logout"
+import Logout from "./Logout";
+import Login from "./Login";
+import Spinner from "./Spinner";
 
- class Auth extends Component {
-   constructor(props) {
-     super(props);
-     // state by default
-     this.state = {
-       isLoggedIn: false,
-       isLoading: false,
-     };
-   }
-   handleLogin = () => {
-     this.setState({
-       isLoggedIn: true,
-     });
-   };
-   handleLogout = () => {
-     this.setState({
-       isLoggedIn: false,
-     });
-   };
-   onLoading = () => {
-     this.setState({
-       isLoading: true,
-     });
-     setTimeout(() => {
-       this.setState({
-         isLoading: true,
-         inLoggedIn: false,
-       });
-     }, 2000);
-   };
-   render() {
-     const button = !this.state.isLoggedIn ? (
-       <Login onLogin={this.handleLogin} />
-     ) : (
-       <Logout onLogout={this.handleLog} />
-     );
+class Auth extends Component {
+  constructor(props) {
+    super(props);
 
-     return (
-       <div className="main">
-         {this.state.isLoading ? <Spinner size={50} /> : button}
-       </div>
-     );
-   }
- }
- export default Auth;
-   
+    this.state = {
+      isLoggedIn: false,
+      isLoading: false,
+    };
+  }
+
+  handleLogIn = () => {
+    this.setState({
+      isLoggedIn: true,
+    });
+    this.loadSpinner();
+  };
+
+  handleLogOut = () => {
+    this.setState({
+      isLoggedIn: false,
+    });
+  };
+
+  loadSpinner = () => {
+    this.setState({
+      isLoading: true,
+    });
+
+    setTimeout(() => {
+      this.setState({
+        isLoading: false,
+      });
+    }, 2000);
+  };
+
+  render() {
+    const button = !this.state.isLoggedIn ? (
+      <Login onLogin={this.handleLogIn} />
+    ) : (
+      <Logout onLogout={this.handleLogOut} />
+    );
+
+    return (
+      <div className="main">
+        {this.state.isLoading ? <Spinner size={50} /> : button}
+      </div>
+    );
+  }
+}
+export default Auth;
