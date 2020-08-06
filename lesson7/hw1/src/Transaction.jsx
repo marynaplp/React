82cafbe8a73006579b536afb180ced4c9cf2aa8a
Maryna <maryna.pylypchenko@gmail.com>
2020-08-06 14:25:36 +0300

    co

--- lesson7/hw1/src/Transaction.jsx
+++ lesson7/hw1/src/Transaction.jsx
@@ -1,2 +1,29 @@
 //describes component which accepts data about the transaction in properties from, to, amount, rate, time. Show data about transaction in coicidence wiht example layout. Dates formatted wiht help of library  moment.js. Numbers create with help of new Inti.NumberFormat, local - en-GB
-   
+   import React from "react";
+import moment from "moment";
+
+const getDate = moment().format("DD MMM");
+
+const Transaction = ({ from, to, amount, rate, time }) => {
+    const times = new Date(time);
+
+    const options = {
+        hour: "numeric",
+        minute: "numeric",
+    };
+
+    const getTime = new Intl.DateTimeFormat("en-GB", options).format(times);
+    const getAmount = new Intl.NumberFormat("en-GB", options).format(amount);
+
+    return (
+        <li className="transaction">
+            <span className="transaction__date">{getDate}</span>
+            <span className="transaction__time">{getTime}</span>
+            <span className="transaction__assets">{`${from} → ${to}`}</span>
+            <span className="transaction__rate">{rate}</span>
+            <span className="transaction__amount">{getAmount}</span>
+        </li>
+    );
+};
+
+export default Transaction;
--- lesson7/hw1/src/TransactionsList.jsx
+++ lesson7/hw1/src/TransactionsList.jsx
@@ -1,1 +1,15 @@
 // describes the Component which accepts a list of transactions in props transactions and shows these transactions with  help  of component Transaction  
+import React, { Component } from "react";
+import Transaction from "./Transaction";
+
+const TransactionsList = ({ transactions }) => {
+  return (
+    <ul className="transactions">
+      {transactions.map((elem) => (
+        <Transaction key={elem.id} {...elem} />
+      ))}
+    </ul>
+  );
+};
+
+export default TransactionsList;
