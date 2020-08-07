17c19339befdbe1c1b351e2dd940d7bb6946b1db
Maryna <maryna.pylypchenko@gmail.com>
2020-08-07 16:23:00 +0300

    co

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -1,32 +1,40 @@
 import React, { Component } from "react";
 
-
 class Life extends Component {
-    constructor(props){
-        super (props);
-        console.log('constructor: good place to create state');
-    }
-    componentDidMount() {
-        console.log("componentDidMount: API calls, subscriptions");
-    }
-    shouldComponentUpdate(nextProps, nextState) { /// for optimization 
-        console.log(nextProps, nextState);
-        console.log("shouldComponentUpdate(nextProps, nextState): decide to render or not to render") 
-        return this.props !== nextProps;
-    //console.log('=> shouldComponentUpdat')
+  constructor(props) {
+    super(props);
+    console.log("constructor: good place to create state");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount: API calls, subscriptions");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(nextProps, nextState);
+    console.log(
+      "shouldComponentUpdate(nextProps, nextState): decide to render or not to render"
+    );
+    return this.props !== nextProps;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevProps, prevState);
+    console.log(
+      "componentDidUpdate(prevProps, prevState): some updates based on new props"
+    );
+  }
+
+  componentWillUnmount() {
+    console.log(
+      "componentWillUnmount(): cleanup before DOM related to component will be removed"
+    );
+  }
+
+  render() {
+    console.log("return React element to build DOM");
+    return null;
+  }
 }
-    componentWillUnmount (){
-        console.log("componentWillUnmount(): cleanup before DOM");
-    }
 
-    componentDidUpdate(prevProps, prevState){
-        console.log(prevProps, prevState)
-        console.log("componentDidUpdate(prevProps, prevState): some updates based on new props");
-    }
-    render (){
-        console.log("return React element to build DOM");
-
-    }
-}
-
-export default Life
+export default Life;
--- lesson8/task1/src/index.jsx
+++ lesson8/task1/src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import "./Life.jsx"
+import Life from "./Life.jsx"
 
 
 
