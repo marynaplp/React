caa3f65e1baba455c6a0ecfb9bfe4d9eabc29955
Maryna <maryna.pylypchenko@gmail.com>
2020-08-13 01:05:33 +0300

    commit

--- lesson11/task2/src/Dialog.jsx
+++ lesson11/task2/src/Dialog.jsx
@@ -1,28 +1,34 @@
 // опишите компоненту которая принимает заголовок в свойстве title и отображает дочерные элементы по центру экрана
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-const Dialog = ({ isOpen, children, title}, onClose) => {
-    if(isOpen){ return null } //передача content
-    return (
-      <div className="dialog">
-        <div className="dialog__heading">
-          <div className="dialog__title">{title}</div>
-          <button className="dialog__close-btn" onClick={onClose}>+</button>
-        </div>
-        <div className="dialog__content">{children}</div>
+const Dialog = ({ isOpen, children, title, onClose }) => {
+  if (!isOpen) {
+    return null;
+  }
+  return (
+    <div className="dialog">
+      <div className="dialog__heading">
+        <div className="dialog__title">{title}</div>
+        <button className="dialog__close-btn" onClick={onClose}>
+          +
+        </button>
       </div>
-    );
-}
+      <div className="dialog__content">{children}</div>
+    </div>
+  );
+};
 
-Dialog.propTypes ={
-    isOpen: PropType.bool,
-    children :PropType.element,
-    title: PropTypes.string,
-    onClose: PropTypes.func.isRequired
-}
-Dialog.defaultProps ={
-    isOpen: false,
-    title: '',
-}
+Dialog.propTypes = {
+  isOpen: PropTypes.bool,
+  children: PropTypes.element.isRequired,
+  title: PropTypes.string,
+  onClose: PropTypes.func.isRequired,
+};
+
+Dialog.defaultTypes = {
+  isOpen: false,
+  title: "",
+};
+
 export default Dialog;
