e2b931c164bbee7df6eddd8ddaffd5f2fa2b7e5a
Maryna <maryna.pylypchenko@gmail.com>
2020-08-18 23:38:02 +0300

    commit

--- lesson13/task1/src/index.jsx
+++ lesson13/task1/src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import App from "./App";
+import Home from "./App";
 
 const rootElement = document.querySelector("#root")
 
-ReactDOM.render(<App />, rootElement)
+ReactDOM.render(<Home />, rootElement)
