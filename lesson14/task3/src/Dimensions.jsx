4be534a6d0be56ba300e5e860f4a3346640edd22
Maryna <maryna.pylypchenko@gmail.com>
2020-08-26 17:45:50 +0300

    commit

--- lesson14/task3/src/Dimensions.jsx
+++ lesson14/task3/src/Dimensions.jsx
@@ -1,56 +1,28 @@
 
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+
 const Dimensions = () => {
-  const[dimensions, setDimensions]= useState({ width: null,
-    height: null,}); 
+  const [dimensions, setDimensions] = useState({
+    width: null,
+    height: null,
+  });
 
-    useEffect(() => {
-      const { innerHeight, innerWidth }= window;
-      setDimensions({width: innerWidth, height: innerHeight});
-      const handleResize= e => {
-        const { innerWidth, innerHeight } = e.target;
+  useEffect(() => {
+    const { innerHeight, innerWidth } = window;
+    setDimensions({ width: innerWidth, height: innerHeight });
+    const handleResize = (e) => {
+      const { innerHeight, innerWidth } = e.target;
       setDimensions({ width: innerWidth, height: innerHeight });
-      }
-      window.addEventListener("resize", handleResize);
-    },[])
-    const {width, height } = dimensions;
-    return (
-      <div className="dimensions">{`${width}px - ${height}px`}</div>
-    );
+    };
+    window.addEventListener("resize", handleResize);
 
-}
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-// class Dimensions extends Component {
-//   state = {
-    
-//   };
-
-//   componentDidMount() {
-//     window.addEventListener("resize", this.onResize);
-
-//     const { innerWidth, innerHeight } = window;
-//     this.setDemensions(innerWidth, innerHeight);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener("resize", this.onResize);
-//   }
-
-//   onResize = (e) => {
-//     const { innerWidth, innerHeight } = e.target;
-//     this.setDemensions(innerWidth, innerHeight);
-//   };
-
-//   setDemensions = (width, height) => {
-//     this.setState({
-//       width,
-//       height,
-//     });
-//     document.title = `${innerWidth} x ${innerHeight}`;
-//   };
-
-//   render() {
-//   }
-// }
+  const { width, height } = dimensions;
+  return <div className="dimensions">{`${width}px - ${height}px`}</div>;
+};
 
 export default Dimensions;
