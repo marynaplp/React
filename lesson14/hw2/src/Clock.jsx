33843600dfb2108e607c853421c61116e9cfb0a8
Maryna <maryna.pylypchenko@gmail.com>
2020-08-27 00:59:38 +0300

    commit

--- lesson14/hw2/src/Clock.jsx
+++ lesson14/hw2/src/Clock.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import "./clock.scss";
+import React, {useEffect, useState } from "react";
+import "./index.scss";
 
 
 const getTimewithOffset = offset => {
@@ -7,27 +7,25 @@ const getTimewithOffset = offset => {
     const utcOffset = currentTime.getTimezoneOffset() / 60
     return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
 }
-class Clock extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-                location: props.location,
-                offset: getTimewithOffset(props.offset).toLocaleTimeString()
 
-            }
-        setInterval(() => {
-            this.setState({
-                offset: getTimewithOffset(props.offset).toLocaleTimeString()
-            });
-        }, 1000);
-    }
-    render() {
+const Clock = ({location, offset}) => {
+    const [clock, setClock] = useState(getTimeWithOffset(offset).toLocaleTimeString());
+
+useEffect(() => {
+    const timeZone = setInterval(() => {
+        setClock (getTimewithOffset(offset).toLocaleTimeString());
+ }, 1000);  
+return () => {
+    clearInterval(timeZone)
+};
+}, []);
+
         return (
            <div className="clock">
-            <div className="clock__location">{this.state.location}</div>
-            <div className="clock__time">{this.state.offset}</div> 
+            <div className="clock__location">{location}</div>
+            <div className="clock__time">{clock}</div> 
             </div >
         );
-    }
 }
+
 export default Clock;
